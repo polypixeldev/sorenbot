eb3fee4581cd3866c45203ac5f2b76f4cfedd022
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-09-26 00:18:50 -0400

    Refactor and add post queries

--- main.ts
+++ main.ts
@@ -13,50 +13,138 @@ const app = new Slack.App({
 
 const parser = new Parser();
 
-app.command("/soren", async ({ client, body, ack }) => {
-  await ack();
+type PostData = {
+  title: string;
+  imageURL: string;
+  date: Date;
+  url: string;
+}
 
-  const feed = await parser.parseURL(Deno.env.get("RSS_FEED")!);
-  const content = feed.items[0].content;
+function escapeRegExp(str: string) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+let cachedFeed: Awaited<ReturnType<typeof parser.parseURL>>;
+let cachedTime: Date;
+// cache for 5 mins
+const CACHE_DURATION = 5 * 60 * 1000;
+
+async function getPostData(query: number | string | Date): Promise<PostData | null> {
+  let feed;
+  if (cachedFeed && new Date().valueOf() - cachedTime.valueOf() < CACHE_DURATION) {
+    feed = cachedFeed;
+  } else {
+    feed = await parser.parseURL(Deno.env.get("RSS_FEED")!);
+    cachedFeed = feed;
+    cachedTime = new Date();
+  }
+  
+  let post;
+  if (typeof(query) === "number") {
+    post = feed.items[query];
+  } else if (query instanceof Date) {
+    post = feed.items.find(p => new Date(p.pubDate!).toDateString() === query.toDateString())
+  } else {
+    post = feed.items.find(p => new RegExp(escapeRegExp(query), 'i').test(p.title!))
+  }
+
+  if (!post) {
+    return null;
+  }
+
+  const content = post.content;
+
   const dom = new jsdom.JSDOM(content);
 
   const imgRegex = /\d{2}-\d{2}/;
   const imageURL = [...dom.window.document.querySelectorAll("img")].find(i => imgRegex.test(i.src)).src;
 
-  const title = feed.items[0].title;
-  const date = new Date(feed.items[0].pubDate!);
+  const title = post.title!;
+  const date = new Date(post.pubDate!);
 
   const url = [...dom.window.document.querySelectorAll("a")].find(a => a.innerHTML.includes("Online")).href;
 
-  await client.chat.postMessage({
-    channel: body.channel_id,
-    text: "Latest Soren Iverson post",
+  const postData = {
+    title,
+    imageURL,
+    date,
+    url
+  }
+
+  return postData;
+}
+
+async function sendPost(data: PostData, channel: string) {
+  await app.client.chat.postMessage({
+    channel,
+    text: "Soren Iverson post",
     blocks: [
       {
         type: "header",
         text: {
           type: "plain_text",
-          text: title!
+          text: data.title
         },
       },
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `Soren Iverson post from ${date.toLocaleDateString()} | <${url}|see newsletter post>`
+          text: `Soren Iverson post from ${data.date.toLocaleDateString()} | <${data.url}|see newsletter post>`
         }
       },
       {
         type: "image",
-        alt_text: title!,
+        alt_text: data.title,
         title: {
           type: "plain_text",
-          text: title!
+          text: data.title
         },
-        image_url: imageURL
+        image_url: data.imageURL
       }
     ]
-  });
+  }).catch(e => console.dir(e));
+}
+
+app.command("/soren", async ({ client, body, ack, command }) => {
+  await ack();
+
+  const args = command.text.split(" ");
+
+  let query: number | string | Date;
+
+  if (args[0] === "latest" || args[0] === "") {
+    query = 0;
+  } else if (!isNaN(Number(args[0]))) {
+    query = Number(args[0])
+  } else if (!isNaN(new Date(command.text).valueOf())) {
+    query = new Date(command.text);
+  } else {
+    query = command.text
+  }
+
+  let data;
+  try {
+    data = await getPostData(query);
+  } catch {
+    await client.chat.postEphemeral({
+      user: body.user_id,
+      channel: body.channel_id,
+      text: "An unknown error occurred when fetching post data :("
+    });
+    return;
+  }
+
+  if (!data) {
+    await client.chat.postEphemeral({
+      user: body.user_id,
+      channel: body.channel_id,
+      text: "Unable to fetch post"
+    });
+    return;
+  }
+
+  await sendPost(data, body.channel_id);
 });
 
 (async () => {
